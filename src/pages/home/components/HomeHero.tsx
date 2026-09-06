3ae4caa969150c332d7ca50649f902d5e32ffd53
yingjie <yjliu@gitlab.cn>
2026-09-06 11:21:52 +0800

    style(home): narrow hero banner

--- src/pages/home/components/HomeHero.tsx
+++ src/pages/home/components/HomeHero.tsx
@@ -71,9 +71,9 @@ export default function HomeHero() {
 
 const styles = StyleSheet.create({
     card: {
-        width: "auto",
+        width: "80%",
+        alignSelf: "center",
         aspectRatio: 2,
-        marginHorizontal: rpx(24),
         marginTop: rpx(12),
         borderRadius: rpx(28),
         overflow: "hidden",
